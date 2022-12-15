c9a6c24721e2c43e44eeac714dc359a78eac0f9d
titoworlddev <titocristian.dev@gmail.com>
2022-12-15 14:48:11 +0100

    Reto 14

--- 2022/reto11.js
+++ 2022/reto11.js
@@ -1,14 +1,36 @@
 function getCompleted(part, total) {
-	let num = part.split(':');
-	let denom = total.split(':');
-	let secNum = num[0] * 60 ** 2 + num[1] * 60 + num[2] * 1;
-	let secDenom = denom[0] * 60 ** 2 + denom[1] * 60 + denom[2] * 1;
-	const getFraction = (decimal) => {
-		for (var i = 1; (decimal * i) % 1 !== 0; i++); 
+  let num = part.split(':');
+  let denom = total.split(':');
+  let secNum = num[0] * 60 ** 2 + num[1] * 60 + num[2] * 1;
+  let secDenom = denom[0] * 60 ** 2 + denom[1] * 60 + denom[2] * 1;
+  const getFraction = (decimal) => {
+    for (var i = 1; (decimal * i) % 1 !== 0; i++);
     return decimal * i + '/' + i;
-	};
-	let fraction = getFraction(secNum / secDenom);
-	return fraction;
+  };
+  let fraction = getFraction(secNum / secDenom);
+  return fraction;
+}
+
+function getCompleted(part, total) {
+  const convert = (time) => {
+    return time
+      .split(':')
+      .reduce((total, part, i) => total + part * 60 ** (2 - i), 0);
+  };
+
+  const partSeconds = convert(part);
+  const totalSeconds = convert(total);
+
+  let a = partSeconds;
+  let b = totalSeconds;
+
+  while (b) [a, b] = [b, a % b];
+  const divisor = a;
+
+  const top = partSeconds / divisor;
+  const bottom = totalSeconds / divisor;
+
+  return `${top}/${bottom}`;
 }
 
 // getCompleted('01:00:00', '03:00:00'); // '1/3'
