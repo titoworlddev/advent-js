34a84e039ace53231f260dda6722b3ad9fd92747
hekhy <titocristian.dev@gmail.com>
2022-12-16 01:39:03 +0100

    inicio reto 15

---
+++ 2022/reto15.js
@@ -0,0 +1,20 @@
+function decorateTree(base) {
+  return []
+}
+
+// Arriba coloca  :     P     R     B     P
+// Si abajo tiene :    P P   B P   R P   B R
+//    R
+//   P B
+//  R B B
+// B P R P
+
+decorateTree('B P R P')
+// [
+// 'R',
+// 'P B',
+// 'R B B',
+// 'B P R P'
+// ]
+
+decorateTree('B B') // ['B', 'B B']
