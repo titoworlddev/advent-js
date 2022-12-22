164b731af99db4c5843e84162f63a7d34ff7d6f7
titoworld.dev <titocristian.dev@gmail.com>
2022-12-22 14:02:02 +0100

    Reto 15 hasta 21

--- 2022/reto15.js
+++ 2022/reto15.js
@@ -1,14 +1,51 @@
 function decorateTree(base) {
+  const cases = {
+    PP: 'P',
+    BP: 'R',
+    PB: 'R',
+    RP: 'B',
+    PR: 'B',
+    BR: 'P',
+    RB: 'P',
+    BB: 'B',
+    RR: 'R',
+    PP: 'P',
+  };
+  let tree = [base];
+  let valid = false;
+
+  // Creamos el arbol
+  while (valid === false) {
+    const preLine = tree[0].split(' ');
+
+    // Agregamos cada letra segun el caso correspondiente
+    let newLine = preLine
+      .map((el, i) => cases[preLine[i - 1] + el])
+      .join(' ').trim();
+
+    // Agregamos la linea al arbol con los espacios
+    tree.unshift(newLine);
+
+    // Validamos si se ha completado el arbol
+    valid = tree[0].replaceAll(' ', '').length 
+          + tree[1].replaceAll(' ', '').length 
+          <= 3;
+  }
+
+  return tree;
 }
 
 // Arriba coloca  :     P     R     B     P
 // Si abajo tiene :    P P   B P   R P   B R
 //    R
+//    ⬇️
 //   P B
+//   ⬇️ ⬇️
 //  R B B
+//  ⬇️ ⬇️ ⬇️
 // B P R P
 
-decorateTree('B P R P')
+decorateTree('B P R P');
 // [
 // 'R',
 // 'P B',
@@ -16,4 +53,8 @@ decorateTree('B P R P')
 // 'B P R P'
 // ]
 
-// decorateTree('B B') // ['B', 'B B']
+// decorateTree('B B')
+// [
+// 'B',
+// 'B B'
+// ]
